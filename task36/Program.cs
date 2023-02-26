483feb0b9363652b26e24f0118e3f4ee5f89c4da
YarosLap <yaroslap86@gmail.com>
2023-02-26 12:45:20 +0300

    first commit

---
+++ task36/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+Console.WriteLine($"Введите количество элементов массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine()); 
+int randomNumber =  RandomNumbers(arraySize, 1, 10);
+Console.WriteLine($"  Сумма элементов, стоящих на нечётных позициях (если первый элемент принимаем за нулевой): {randomNumber}");
+
+int RandomNumbers(int ElementNumbers, int min, int max)
+  {
+  int[] randomNumber = new int[ElementNumbers];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
+    Console.Write("[ ");
+    for (int i = 0; i < randomNumber.Length; i++ ){
+      randomNumber[i] = new Random().Next(-99, 100);
+
+      Console.Write(randomNumber[i] + " ");
+
+      if (i % 2 != 0)
+      {
+        sumElements = sumElements + randomNumber[i];
+      }
+    }
+    Console.Write("]");
+  return sumElements;
+  }
+
---
+++ task38/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество элементов массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+double[] number = new double[arraySize];
+
+FillArrayRandomNumbers(number);
+Console.WriteLine("Сгенерирован следующий массив: ");
+PrintArray(number);
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int a = 0; a < number.Length; a++)
+{
+    if (number[a] > max)
+        {
+            max = number[a];
+        }
+    if (number[a] < min)
+        {
+            min = number[a];
+        }
+}
+
+Console.WriteLine($"Всего в массиве {number.Length} чисел. Максимальное = {max}, Минимальное = {min}");
+Console.WriteLine($"Разность между максимальным и минимальным = {max - min}");
+
+void FillArrayRandomNumbers(double[] number)
+{
+    for(int i = 0; i < number.Length; i++)
+        {
+            number[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] number)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < number.Length; i++)
+        {
+            Console.Write(number[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
